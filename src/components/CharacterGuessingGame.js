76039529fa086c91aec88febf73970ae15a156d1
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2026-01-25 23:54:27 +0100

    added game modes

--- src/components/CharacterGuessingGame.js
+++ src/components/CharacterGuessingGame.js
@@ -5,6 +5,7 @@ import CharacterGuessingGameResult from "./CharacterGuessingGameResult";
 
 function CharacterGuessingGame() {
     const [actor, setActor] = useState("");
+    const [mode, setMode] = useState("Easy");
     const [isLoading, setIsLoading] = useState(false);
     const [hasSearched, setHasSearched] = useState(false);
     const [result, setResult] = useState([]);
@@ -14,6 +15,10 @@ function CharacterGuessingGame() {
     const handleActorChange = (e) => {
         setActor(e.currentTarget.value)
     };
+
+    const handleModeChange = (e) => {
+        setMode(e.currentTarget.value);
+    }
 
     const playGame = async(e) => {
         e.preventDefault();
@@ -41,31 +46,48 @@ function CharacterGuessingGame() {
                 return;     
             }
 
+            let filteredCredits = [];
 
-            const filteredCredits = credits.cast.filter(movie =>
+            filteredCredits = credits.cast.filter(movie =>
                 !movie.genre_ids.includes(99) &&
                 !movie.genre_ids.includes(10770) &&
-                movie.original_title !== "Final Cut: Hölgyeim és uraim" &&
-                !movie.character.includes("uncredited") &&
-                movie.vote_count > 140
-                )
-                .map(movie => 
-                    ({ 
+                !movie.character.includes("uncredited")
+                ).map(movie => ({ 
                     id: movie.id,
                     title: movie.title,
                     character: movie.character,
                     poster_path: movie.poster_path,
-                    release_date: movie.release_date
-                    }));
-            
+                    release_date: movie.release_date,
+                    popularity: movie.popularity,
+                    vote_count: movie.vote_count
+            }));
+
             if (filteredCredits.length === 0) {
                 setResult([]); 
                 setHasSearched(true);
                 setIsLoading(false);
                 return;
-            }
+            };
+
+            let modeCredits = [];
+
+            if (mode === "Easy") {
+            modeCredits = filteredCredits.filter(movie => movie.popularity > 9)
+            } else if (mode === "Medium") {
+            modeCredits = filteredCredits.filter(movie => movie.popularity > 6)
+            } else if (mode === "Hard") {
+            modeCredits = filteredCredits.filter(movie => movie.vote_count > 140)
+            } else if (mode === "Random 10") {
+                const eligibleCredits = filteredCredits.filter(movie => movie.vote_count > 140)
+
+                modeCredits = [...eligibleCredits].sort(() => 0.5 - Math.random()).slice(0, 10);
+            };
         
-        setResult(filteredCredits);
+        if (modeCredits.length === 0) {
+            setResult([]);
+        } else {
+            setResult(modeCredits);
+        }
         setIsLoading(false);
         setHasSearched(true);
         } catch (err) {
@@ -103,6 +125,8 @@ function CharacterGuessingGame() {
             <CharacterGuessingGameForm
             actor={actor}
             handleActorChange={handleActorChange}
+            mode={mode}
+            handleModeChange={handleModeChange}
             playGame={playGame} />
             <CharacterGuessingGameResult 
             result={result}
--- src/components/CharacterGuessingGameForm.js
+++ src/components/CharacterGuessingGameForm.js
@@ -1,9 +1,10 @@
 
-function CharacterGuessingGameForm({ actor, handleActorChange, playGame }) {
+function CharacterGuessingGameForm({ actor, handleActorChange, playGame, mode, handleModeChange }) {
 
     return (
         <div className="form-container">
-            <p>Type in the name of an actor or actress with who you want to play the game.</p>
+            <p>Type in the name of an actor or actress to play the game.</p>
+            <p>The easier the mode the more popular the movies. If you can't find any movies, try a harder mode.</p>
             <form onSubmit={playGame}>
                 <input 
                 type="text"
@@ -12,6 +13,16 @@ function CharacterGuessingGameForm({ actor, handleActorChange, playGame }) {
                 placeholder="Actor Name"
                 aria-label="Actor Name"
                 required />
+                <select
+                name="gamemode"
+                value={mode}
+                onChange={handleModeChange}
+                aria-label="Game Mode" >
+                    <option value="Easy">Easy</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Hard">Hard</option>
+                    <option value="Random 10">Random 10</option>    
+                </select>
                 <button
                 type="submit"
                 aria-label="Play Game" >
