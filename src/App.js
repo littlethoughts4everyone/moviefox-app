7c2a0d8cd0d1cd167ad089fa431b959fe5b71a3d
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2026-06-09 22:06:27 +0200

    imported and added home component as index element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AppLayout from './AppLayout';
-import ActorMutualMovies from './components/ActorMutualMovies';
-import RevenueLists from './components/RevenueLists';
-import PeopleTrivia from './components/PeopleTrivia';
+import Home from './components/Home';
 import FindMovies from './components/FindMovies';
+import RevenueLists from './components/RevenueLists';
+import People from './components/People';
 import CharacterGuessingGame from './components/CharacterGuessingGame';
 import './App.css';
 import "charts.css/dist/charts.css";
@@ -13,10 +13,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AppLayout/>}>
-          <Route path="mutualmovies" element={<ActorMutualMovies />} />
-          <Route path="revenuelists" element={<RevenueLists />} />
-          <Route path="peopletrivia" element={<PeopleTrivia />} />
+          <Route index element={<Home />} />
           <Route path="findmovies" element={<FindMovies />} />
+          <Route path="revenuelists" element={<RevenueLists />} />
+          <Route path="people" element={<People />} />
           <Route path="characterguessinggame" element={<CharacterGuessingGame />} />
         </Route>
       </Routes>
